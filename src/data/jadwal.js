6e2023490b43722bae15190fc3e13901aa98fe02
yusuf habib <yusufhabib@yusufs-Mac-mini.local>
2025-10-24 17:13:24 +0800

    refactor: data jadwal

--- src/data/jadwal.js
+++ src/data/jadwal.js
@@ -1,54 +1,53 @@
 export const jadwalSMA = {
   "Kelas 10": {
-    "Senin": ["Bahasa Inggris", "Biologi"],
-    "Selasa": ["Kimia", "Sosiologi", "Agama Islam"],
-    "Rabu": ["Matematika", "Bahasa Indonesia", "Sejarah"],
-    "Kamis": ["PKN", "Bahasa Inggris", "Biologi", "29 Karakter"],
-    "Jumat": ["PJOK", "SBD", "Bahasa Indonesia"],
-    "Sabtu": ["Fisika", "TIK", "Ekonomi"]
+    Senin: ["Bahasa Inggris", "Biologi"],
+    Selasa: ["Kimia", "Sosiologi", "Agama Islam"],
+    Rabu: ["Matematika", "Bahasa Indonesia", "Sejarah"],
+    Kamis: ["PKN", "Bahasa Inggris Peminatan", "Biologi", "29 Karakter"],
+    Jumat: ["PJOK", "SBD", "Bahasa Indonesia"],
+    Sabtu: ["Fisika", "TIK", "Ekonomi"],
   },
   "Kelas 11": {
-    "Senin": ["Agama Islam", "Fisika"],
-    "Selasa": ["SBD", "PKN", "Matematika"],
-    "Rabu": ["PJOK", "29 Karakter", "Biologi"],
-    "Kamis": ["Bahasa Indonesia", "Biologi", "Ekonomi"],
-    "Jumat": ["Sosiologi", "Bahasa Inggris"],
-    "Sabtu": ["Sejarah", "Bahasa Indonesia", "TIK"]
+    Senin: ["Agama Islam", "Fisika"],
+    Selasa: ["SBD", "PKN", "Matematika"],
+    Rabu: ["PJOK", "29 Karakter", "Biologi"],
+    Kamis: ["Bahasa Indonesia", "Biologi", "Ekonomi"],
+    Jumat: ["Sosiologi", "Bahasa Inggris"],
+    Sabtu: ["Sejarah", "Bahasa Indonesia", "TIK"],
   },
   "Kelas 12": {
-    "Senin": ["Biologi", "TIK"],
-    "Selasa": ["PJOK", "Bahasa Indonesia", "Ekonomi"],
-    "Rabu": ["29 Karakter", "SBD", "Matematika"],
-    "Kamis": ["Sosiologi", "PKN", "Fisika"],
-    "Jumat": ["Bahasa Indonesia", "Biologi", "Sejarah"],
-    "Sabtu": ["Agama Islam", "Sosiologi", "Bahasa Inggris"]
-  }
-}
-
+    Senin: ["Biologi", "TIK"],
+    Selasa: ["PJOK", "Bahasa Indonesia", "Ekonomi"],
+    Rabu: ["29 Karakter", "SBD", "Matematika"],
+    Kamis: ["Sosiologi", "PKN", "Fisika"],
+    Jumat: ["Bahasa Indonesia", "Biologi", "Sejarah"],
+    Sabtu: ["Agama Islam", "Sosiologi", "Bahasa Inggris"],
+  },
+};
 
 export const jadwalSMP = {
-    "Kelas 7": {
-      "Senin": ["Matematika", "PKN"],
-      "Selasa": ["IPS", "Bahasa Indonesia", "Seni Rupa"],
-      "Rabu": ["Prakarya", "Bahasa Inggris", "IPA"],
-      "Kamis": ["Bahasa Inggris", "Matematika", "Agama Islam"],
-      "Jumat": ["29 Karakter", "IPS", "IPA"],
-      "Sabtu": ["PJOK", "TIK", "Bahasa Indonesia"]
-    },
-    "Kelas 8": {
-      "Senin": ["Bahasa Indonesia", "Matematika"],
-      "Selasa": ["Bahasa Indonesia", "IPS", "PKN"],
-      "Rabu": ["IPA", "Matematika", "Seni Rupa"],
-      "Kamis": ["Prakarya", "Bahasa Inggris", "TIK"],
-      "Jumat": ["PJOK", "Bahasa Inggris", "IPS"],
-      "Sabtu": ["29 Karakter", "Agama Islam", "IPA"]
-    },
-    "Kelas 9": {
-      "Senin": ["IPA", "Seni Rupa"],
-      "Selasa": ["PJOK", "Matematika", "Bahasa Indonesia"],
-      "Rabu": ["PKN", "IPS", "Prakarya"],
-      "Kamis": ["29 Karakter", "IPA", "Matematika"],
-      "Jumat": ["Bahasa Inggris", "TIK", "Bahasa Indonesia"],
-      "Sabtu": ["IPS", "Bahasa Inggris", "Agama Islam"]
-    }
-  }
+  "Kelas 7": {
+    Senin: ["Matematika", "PKN"],
+    Selasa: ["IPS", "Bahasa Indonesia", "Seni Rupa"],
+    Rabu: ["Prakarya", "Bahasa Inggris", "IPA"],
+    Kamis: ["Bahasa Inggris", "Matematika", "Agama Islam"],
+    Jumat: ["29 Karakter", "IPS", "IPA"],
+    Sabtu: ["PJOK", "TIK", "Bahasa Indonesia"],
+  },
+  "Kelas 8": {
+    Senin: ["Bahasa Indonesia", "Matematika"],
+    Selasa: ["Bahasa Indonesia", "IPS", "PKN"],
+    Rabu: ["IPA", "Matematika", "Seni Rupa"],
+    Kamis: ["Prakarya", "Bahasa Inggris", "TIK"],
+    Jumat: ["PJOK", "Bahasa Inggris", "IPS"],
+    Sabtu: ["29 Karakter", "Agama Islam", "IPA"],
+  },
+  "Kelas 9": {
+    Senin: ["IPA", "Seni Rupa"],
+    Selasa: ["PJOK", "Matematika", "Bahasa Indonesia"],
+    Rabu: ["PKN", "IPS", "Prakarya"],
+    Kamis: ["29 Karakter", "IPA", "Matematika"],
+    Jumat: ["Bahasa Inggris", "TIK", "Bahasa Indonesia"],
+    Sabtu: ["IPS", "Bahasa Inggris", "Agama Islam"],
+  },
+};
