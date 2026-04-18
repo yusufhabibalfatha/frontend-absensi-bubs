ca571ddf73d504e8d4a52cdccafcf13381233eae
m1 yusuf <y.habib.alfatha@gmail.com>
2026-04-18 21:57:31 +0800

    Refactor main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import BerandaLayout from "./layouts/BerandaLayout";
 import NotFound from "./components/NotFound";
 import Login from "./components/Login";
@@ -24,122 +24,120 @@ import "./style/styles.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<BerandaLayout />}>
-          <Route index element={<Beranda />} />
-        </Route>
+    <Routes>
+      <Route path="/" element={<BerandaLayout />}>
+        <Route index element={<Beranda />} />
+      </Route>
 
-        <Route path="/pilih" element={<BerandaLayout />}>
-          <Route index element={<PilihMapel />} />
-        </Route>
+      <Route path="/pilih" element={<BerandaLayout />}>
+        <Route index element={<PilihMapel />} />
+      </Route>
 
-        <Route path="/absen" element={<BerandaLayout />}>
-          <Route index element={<AbsensiSiswa />} />
-        </Route>
+      <Route path="/absen" element={<BerandaLayout />}>
+        <Route index element={<AbsensiSiswa />} />
+      </Route>
 
-        <Route path="/kegiatan" element={<BerandaLayout />}>
-          <Route index element={<PilihKegiatan />} />
-        </Route>
+      <Route path="/kegiatan" element={<BerandaLayout />}>
+        <Route index element={<PilihKegiatan />} />
+      </Route>
 
-        <Route path="/kegiatan/kelas" element={<BerandaLayout />}>
-          <Route index element={<PilihKelas />} />
-        </Route>
+      <Route path="/kegiatan/kelas" element={<BerandaLayout />}>
+        <Route index element={<PilihKelas />} />
+      </Route>
 
-        <Route path="/kegiatan/kamar" element={<BerandaLayout />}>
-          <Route index element={<PilihKamar />} />
-        </Route>
+      <Route path="/kegiatan/kamar" element={<BerandaLayout />}>
+        <Route index element={<PilihKamar />} />
+      </Route>
 
-        <Route path="/kegiatan/absen" element={<BerandaLayout />}>
-          <Route index element={<AbsensiKegiatan />} />
-        </Route>
+      <Route path="/kegiatan/absen" element={<BerandaLayout />}>
+        <Route index element={<AbsensiKegiatan />} />
+      </Route>
 
-        <Route path="/download-qr" element={<BerandaLayout />}>
-          <Route index element={<DownloadQR />} />
-        </Route>
+      <Route path="/download-qr" element={<BerandaLayout />}>
+        <Route index element={<DownloadQR />} />
+      </Route>
 
-        {/* Route untuk Classroom System */}
-        <Route path="/login" element={<Login />} />
+      {/* Route untuk Classroom System */}
+      <Route path="/login" element={<Login />} />
 
-        <Route
-          path="/classroom/guru"
-          element={
-            <ProtectedRoute role="GURU">
-              <DashboardGuru />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/classroom/siswa"
-          element={
-            <ProtectedRoute role="SISWA">
-              <DashboardSiswa />
-            </ProtectedRoute>
-          }
-        />
+      <Route
+        path="/classroom/guru"
+        element={
+          <ProtectedRoute role="GURU">
+            <DashboardGuru />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/classroom/siswa"
+        element={
+          <ProtectedRoute role="SISWA">
+            <DashboardSiswa />
+          </ProtectedRoute>
+        }
+      />
 
-        <Route
-          path="/presensi-siswa"
-          element={
-            <ProtectedRoute role="SISWA">
-              <PresensiSiswa />
-            </ProtectedRoute>
-          }
-        />
+      <Route
+        path="/presensi-siswa"
+        element={
+          <ProtectedRoute role="SISWA">
+            <PresensiSiswa />
+          </ProtectedRoute>
+        }
+      />
 
-        <Route
-          path="/rekap-presensi-guru"
-          element={
-            <ProtectedRoute role="GURU">
-              <RekapPresensiGuru />
-            </ProtectedRoute>
-          }
-        />
-        {/* ROUTES BARU UNTUK TUGAS & MATERI */}
-        <Route
-          path="/classroom/tugas-guru"
-          element={
-            <ProtectedRoute role="GURU">
-              <TugasGuru />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/classroom/materi-guru"
-          element={
-            <ProtectedRoute role="GURU">
-              <MateriGuru />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/classroom/tugas-siswa"
-          element={
-            <ProtectedRoute role="SISWA">
-              <TugasSiswa />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/classroom/materi-siswa"
-          element={
-            <ProtectedRoute role="SISWA">
-              <MateriSiswa />
-            </ProtectedRoute>
-          }
-        />
+      <Route
+        path="/rekap-presensi-guru"
+        element={
+          <ProtectedRoute role="GURU">
+            <RekapPresensiGuru />
+          </ProtectedRoute>
+        }
+      />
+      {/* ROUTES BARU UNTUK TUGAS & MATERI */}
+      <Route
+        path="/classroom/tugas-guru"
+        element={
+          <ProtectedRoute role="GURU">
+            <TugasGuru />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/classroom/materi-guru"
+        element={
+          <ProtectedRoute role="GURU">
+            <MateriGuru />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/classroom/tugas-siswa"
+        element={
+          <ProtectedRoute role="SISWA">
+            <TugasSiswa />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/classroom/materi-siswa"
+        element={
+          <ProtectedRoute role="SISWA">
+            <MateriSiswa />
+          </ProtectedRoute>
+        }
+      />
 
-        <Route
-          path="/classroom/submissions/:id"
-          element={
-            <ProtectedRoute role="GURU">
-              <SubmissionList />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+      <Route
+        path="/classroom/submissions/:id"
+        element={
+          <ProtectedRoute role="GURU">
+            <SubmissionList />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
